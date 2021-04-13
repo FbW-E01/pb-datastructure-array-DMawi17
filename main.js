83343bf624976e6f2aaedfe1a327d1de7009b4e5
DMawi17 <asegda17@gmail.com>
2021-04-13 11:55:53 +0200

    Array

---
+++ main.js
@@ -0,0 +1,14 @@
+// Declare a variable named myArray and store an array that contains 5 elements. Print the array.
+
+let myArray = ["red", "blue", "green", "yellow", "purple"];
+console.log(myArray);
+
+// Access the 3rd element in the array and save that to another variable named thirdElement. Print the thirdElement.
+
+let thirdElement = myArray[2];
+console.log(thirdElement);
+
+// Reassign the fourth element of the array and give it a value of "fourth element". Print the array once more.
+
+myArray[3] = "fourth element";
+console.log(myArray);
